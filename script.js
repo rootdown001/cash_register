8da18c3b5d863bc6e31def9800e8b570b16911aa
Lance Anderson <lance.a.585@gmail.com>
2022-10-17 20:04:08 -0700

    Fix
    Fix infinate loop (not updateing index correctly)

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function checkCashRegister(price, cash, cid) {
     let workingChange;
 // declare obj for answer object
     let objAnswer;
+// declare objChange for change object
+    let objChange = "finish this";
 // declare workingCid so cid can be changed as we go along
     let workingCid = [];
     workingCid = [...cid];
@@ -16,7 +18,7 @@ function checkCashRegister(price, cash, cid) {
     let arrOptions = [
         {status: "INSUFFICIENT_FUNDS", change: []},
         {status: "CLOSED", change: cid},
-        {status: "OPEN", change: objAnswer}
+        {status: "OPEN", change: objChange}
     ]
 
 // totalChange = cash - price
@@ -85,33 +87,48 @@ function checkCashRegister(price, cash, cid) {
     } else {
         // while still change to calculate AND not past end (reverse) of cid array...
         index = multipleFunc(workingChange);
-        while ((workingChange > 0) && ((workingCid.length - 1 - index) > 0)) {
-  
+        while ((workingChange > 0) && ((workingCid.length - 1 - index) >= 0)) {
+            console.log("start loop");
 
             //console.log("multiple: " + (workingCid[workingCid.length - 1 - index]))
             //console.log("cid index: " + (workingCid.length - 1 - index))
             // see if this multiple has change to give
             
             if (((workingCid[workingCid.length - 1 - index])[1]) >= workingChange) {
+                console.log("enters if stmt");
                 // decrease cid denomination by 1 coin
                 (workingCid[workingCid.length - 1 - index])[1] = ((workingCid[workingCid.length - 1 - index])[1]).toFixed(2) - (multipleArr[index][1]).toFixed(2);
                 console.log(workingCid);
                 // decrease working change by 1 coin
-                console.log("workingChange before decrease: " + workingChange);
-                console.log("amt subtracted: " + multipleArr[index][1]);
+                // console.log("workingChange before decrease: " + workingChange);
+                // console.log("amt subtracted: " + multipleArr[index][1]);
                 workingChange = workingChange - (multipleArr[index][1]);
                 x = workingChange.toFixed(2);
                 workingChange = x;
                 console.log("workingChange after decrease: " + workingChange);
+                index = multipleFunc(workingChange);
+            } else {
+                console.log("before index++: " + index);
+                index++;
+                console.log("after index++: " + index);
             }
-            
-            // call multipleFunc to find first multiple
-            index = multipleFunc(workingChange);
-            
-
         }
-    
-    
+        //while loop ended
+
+        // see if workingChange is 0 or not
+        // if all change given & more left in drawer
+        if (workingChange == 0 && (sumCid() > 0)) {
+            objAnswer = arrOptions[2];
+        } 
+        
+        else if (workingChange == 0 && (sumCid() = 0)) {
+            objAnswer = arrOptions[1];
+        } else {
+            objAnswer = arrOptions[0];
+        }
+        
+       
+
     }
 
 
@@ -122,4 +139,17 @@ function checkCashRegister(price, cash, cid) {
     return objAnswer;
   }
   
-  checkCashRegister(19.4, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  // {status: "OPEN", change: [["QUARTER", 0.5]]}
+  //checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  
+  // {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
+  //checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+
+  // WORKS {status: "INSUFFICIENT_FUNDS", change: []} 
+  //checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+
+  // {status: "INSUFFICIENT_FUNDS", change: []}
+  checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+
+  // {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
+  //checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
